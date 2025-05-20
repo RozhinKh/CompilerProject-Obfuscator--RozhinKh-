4f3e7a2e50f83fd978abee9176aaaa40f81a3025
kimia <kimiamzf10@gmail.com>
2025-05-20 20:20:27 +0330

    added functions to store names with cache key and clear cache

--- obfuscations/ast_rename.py
+++ obfuscations/ast_rename.py
@@ -31,3 +31,19 @@ def generate_obfuscated_name(category_key):
     rename_name_counters[category_key] += 1
     prefix = PREFIXES[category_key]
     return f"{prefix}{rename_name_counters[category_key]}"
+
+def get_or_create_renamed_name(original_name, category_key):
+    if original_name in RESERVED_NAMES:
+        return original_name
+
+    actual_category_key_for_cache = category_key if category_key == 'func' else 'var'
+    cache_key = (original_name, actual_category_key_for_cache)
+
+    if cache_key not in rename_name:
+        rename_name[cache_key] = generate_obfuscated_name(actual_category_key_for_cache)
+    return rename_name[cache_key]
+
+
+def reset_renaming_state():
+    rename_name.clear()
+    for key in rename_name_counters: rename_name_counters[key] = 0
